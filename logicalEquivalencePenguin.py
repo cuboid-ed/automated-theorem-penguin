7716cd209181e4ce06a75810b157eefbc543c1cb
cuboided <85214314+cuboid-ed@users.noreply.github.com>
2022-12-25 15:56:33 -0500

    Recognizes negatable statements, most nested one

--- logicalEquivalencePenguin.py
+++ logicalEquivalencePenguin.py
@@ -18,11 +18,35 @@ if "ㄱ(" in statement:
     push_negation = True
     print("Negation can be pushed in")
     
-# recognize each place that can get negated 
-for char in statement:
-    if char == "ㄱ":
-        negation_char = True
-    if negation_char = True and char == "(": # start storing 
+# recognize each place that can get negated
+negatable_statements = []
+negatable_statements_start = []
+negatable_start = 0 
+negatable = ""
+negation_char = False
+store_negated_part = False 
+for i in range(0, len(statement)):
+    char = statement[i]
+    
+    if store_negated_part == True and char == ")":
+        negatable_statements.append(negatable)
+        store_negated_part = False
+        negatable = ""
+        negatable_statements_start.append(negatable_start)
+    elif store_negated_part == True:
+        negatable = negatable + char
+    
+    if negation_char == True and char == "(": # start storing
+        negatable_start = i + 1 # where the negatable starts 
         store_negated_part = True
-    elif negation_char = True and not(char == "("): # get out of can negate mode 
-        negation_char = False 
+        negation_char = False
+        negatable = ""
+    elif negation_char == True and not(char == "("): # get out of can negate mode 
+        negation_char = False
+        store_negated_part = False
+
+    if char == "ㄱ": # start negation mode 
+        negation_char = True 
+
+print(negatable_statements)
+print(negatable_statements_start)

6d7d49605068066c79aa4626fb0fe0118673df54
cuboided <85214314+cuboid-ed@users.noreply.github.com>
2022-12-25 14:40:13 -0500

    Checking if negation can be pushed in

--- logicalEquivalencePenguin.py
+++ logicalEquivalencePenguin.py
@@ -4,6 +4,18 @@
 
 statement = input("input: ")
 
-print(statement)
+#logical equivalences
+double_negation = False 
 
 # implementing the double negation
+if "ㄱㄱ" in statement:
+    double_negation = True
+
+
+# check if negation can be pushed in 
+push_negation = False 
+if "ㄱ(" in statement:
+    push_negation = True
+    print("Negation can be pushed in")
+    
+# push the negation in now

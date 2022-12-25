b9b35460b7f45b14425021f6e5c6c894729c9966
cuboided <85214314+cuboid-ed@users.noreply.github.com>
2022-12-25 15:31:00 -0500

    The wrong method for recognizing each place that can get negated

--- logicalEquivalencePenguin.py
+++ logicalEquivalencePenguin.py
@@ -18,4 +18,11 @@ if "ㄱ(" in statement:
     push_negation = True
     print("Negation can be pushed in")
     
-# push the negation in now
+# recognize each place that can get negated 
+for char in statement:
+    if char == "ㄱ":
+        negation_char = True
+    if negation_char = True and char == "(": # start storing 
+        store_negated_part = True
+    elif negation_char = True and not(char == "("): # get out of can negate mode 
+        negation_char = False 

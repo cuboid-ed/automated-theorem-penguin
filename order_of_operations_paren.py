23302f2c283648357102381a14892685b3042de1
cuboided <85214314+cuboid-ed@users.noreply.github.com>
2022-12-28 21:33:07 -0500

    before deciding to the "building up" method! probably will delete most

--- order_of_operations_paren.py
+++ order_of_operations_paren.py
@@ -7,3 +7,27 @@
 → implies
 ↔ biconditional 
 '''
+
+
+statement = input("input: ")
+#statement = "ㄱ(aΛq)ΛㄱqΛㄱ(ㄱ(aⅤq)ΛpΛㄱ((pΛq)Ⅴs)Λp)Λㄱ(s→q)Λㄱ((aⅤ(aΛb))Λ(pⅤq)Ⅴ(pΛq)Λ(pⅤs)Λ(pⅤk))" 
+#statement = "ㄱ(aΛbⅤc)"
+print(statement) # checking 
+
+def addParenForParen(operators, statement):
+    '''
+    separates string given a list of operators, statement, assuming all
+    variables are 1 letter long
+    operators = ['ㄱ', 'Λ', 'Ⅴ', '→', '↔']
+    '''
+    paren = []
+    for i in 
+    # controlling parenthesies list 
+        if char == "(": 
+            paren.append("(")  
+        elif char == ")":
+            paren.pop()
+
+def addParenForOpearator(operator):
+    ''' adds parenthesies for a given operator 
+    '''
